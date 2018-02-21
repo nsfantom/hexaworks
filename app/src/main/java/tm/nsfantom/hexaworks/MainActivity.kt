d37f36fe8b30acc6542070f7bf4790417da2ccaf
Mykola Tatarenkov <tatarenkov.ms@gbksoft.com>
2018-02-21 13:57:33 +0200

    add files

---
+++ app/src/main/java/tm/nsfantom/hexaworks/MainActivity.kt
@@ -0,0 +1,12 @@
+package tm.nsfantom.hexaworks
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
